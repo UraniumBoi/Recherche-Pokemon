9f297e03d9d83428d6b8dc8389f2d16d4f6855e6
UraniumBoi <73393118+UraniumBoi@users.noreply.github.com>
2022-02-24 18:37:54 +0100

    Recherche de Pokémon v2.0

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import webbrowser
 
+#-----------------------------------------------
+#------------------Fonction---------------------
 
 def open_pokepedia():
     webbrowser.open_new_tab("https://www.pokepedia.fr/" + saisie_fr.get())
@@ -26,70 +28,157 @@ def open_atk_bb():
 def open_types_bb():
     webbrowser.open_new_tab("https://bulbapedia.bulbagarden.net/wiki/Type")
 
+#-----------------------------------------------
+#------------------Var Tk-----------------------
 
 window = Tk()
 
-window.title("Recherche page Pokémon")
+#-----------------------------------------------
+#------------------Perso Window-----------------
+
+window.title("Recherche page Pokémon V2")
 window.geometry("720x600")
-window.minsize(600, 600)
-window.iconbitmap("logo.ico")
+window.minsize(720, 600)
+#window.maxsize(720, 600)
+window.iconbitmap("img/logo.ico")
 window.config(background='brown')
+
+#-----------------------------------------------
+#------------------Frame------------------------
 
 frame = Frame(
     window,
-    bg='brown'
 )
 
-search = Frame(
+
+#-----------------------------------------------
+#------------------Variable---------------------
+
+quit = PhotoImage(file='img/deconnexion.png')
+search = PhotoImage(file='img/search.png')
+furret = PhotoImage(file='img/furret.png')
+bar_blue = PhotoImage(file='img/bar_blue.png')
+
+#-----------------------------------------------
+#------------------Canvas-----------------------
+
+canvas1 = Canvas(
     window,
+    width=720,
+    height=600,
+    highlightthickness=0,
     bg='brown'
 )
 
-text = Label(
+#-----------------------------------------------
+#------------------Entity-----------------------
+
+canvas1.create_image(
+    100,
+    20,
+    image=furret,
+    anchor=NW
+)
+
+canvas1.create_text(
+    360,
+    60,
+    text="Recherche Pokémon",
+    font=("Arial", 30)
+)
+
+canvas1.create_image(
+    0,
+    0,
+    anchor=NW,
+    image=bar_blue
+)
+
+#-----------------------------------------------
+#------------------Action-----------------------
+
+btn_quit = Button(
     window,
-    text="Recherche de Pokémon sur Internet",
-    font=("Calibri", 20),
-    bg='brown'
+    image=quit,
+    bg='#06dee5',
+    borderwidth=0,
+    command=window.quit
 )
-
 button_fr = Button(
-    search,
-    text="Pokepedia",
-    font=("Arial", 20),
-    bg='red',
-    command=open_pokepedia
+    window,
+    image=search,
+    fg='brown',
+    borderwidth=0,
+    command=open_pokepedia   
 )
 
 saisie_fr = Entry(
-    search,
+    window,
     font=("Arial", 20)
 )
 
 button_en = Button(
-    frame,
-    text="Bulbapedia",
-    font=("Arial", 20),
-    bg='red',
+    window,
+    image=search,
+    fg='brown',
+    borderwidth=0,
     command=open_bulbapedia
 )
 
 saisie_en = Entry(
-    frame,
+    window,
     font=("Arial", 20)
 )
 
-text.pack(side=TOP, pady=50)
-button_fr.pack(side=RIGHT)
-saisie_fr.pack(side=LEFT)
-button_en.pack(side=RIGHT)
-saisie_en.pack(side=LEFT)
-search.pack(expand=YES, fill=Y)
-frame.pack(expand=YES, fill=Y)
+#-----------------------------------------------
+#-----------------------------------------------
+
+canvas1.create_window(
+    480,
+    200,
+    anchor=NW,
+    window=button_fr
+)
+
+canvas1.create_window(
+    150,
+    200,
+    anchor=NW,
+    window=saisie_fr
+)
+
+canvas1.create_window(
+    480,
+    350,
+    anchor=NW,
+    window=button_en
+)
+
+canvas1.create_window(
+    150,
+    350,
+    anchor=NW,
+    window=saisie_en
+)
+
+canvas1.create_window(
+    640,
+    525,
+    anchor=NW,
+    window=btn_quit
+)
+#-----------------------------------------------
+#------------.pack and .pack--------------------
+
+canvas1.pack(fill=BOTH, expand=YES)
+frame.pack(expand=YES)
+
+#-----------------------------------------------
+#------------------menu_bar---------------------
 
 menu_bar = Menu(window)
 pp = Menu(menu_bar, tearoff=0)
-bb = Menu(menu_bar, tearoff=1)
-quit = Menu(menu_bar, tearoff=2)
+bb = Menu(menu_bar, tearoff=0)
 pp.add_command(label="Pokemon", command=open_pokemon_pp)
 pp.add_command(label="Attaques", command=open_atk_pp)
 pp.add_command(label="Types", command=open_types_pp)
@@ -98,7 +187,10 @@ bb.add_command(label="Attaques", command=open_atk_bb)
 bb.add_command(label="Types", command=open_types_bb)
 menu_bar.add_cascade(label="Poképedia", menu=pp)
 menu_bar.add_cascade(label="Bulbapedia", menu=bb)
-menu_bar.add_cascade(label="Quitter", command=window.quit)
+
+#-----------------------------------------------
+#------------------Launch-----------------------
 
 window.config(menu=menu_bar)
 window.mainloop()
+
